9fded7617a7ccc5a69bd45a5bbac20769b935839
ZeroGold <77712091+ZeroGold@users.noreply.github.com>
2024-06-02 12:18:15 -0400

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.db import get_client
-from src.db import client_ping
-import threading
-import logging
+from src.db import get_mongo_client
 from src.dtos.ISayHelloDto import ISayHelloDto
-import time
-import asyncio
+from bson import ObjectId
+from pymongo.operations import SearchIndexModel
+
 
 #comment
-conn = None                
-def createDbConnection():
-  conn = get_client()
-  return conn
+#conn = None                
+#def createDbConnection():
+#  conn = get_mongo_client()
+#  return conn
                 
 app = FastAPI()
 
-x = threading.Thread(target=createDbConnection)
+
+
+
+##CORS
 
 origins = [
     "http://localhost.tiangolo.com",
@@ -35,7 +36,42 @@ app.add_middleware(
 )
 
 
+###SEARCH INDEX
+def set_search_atlas_index():
 
+    try:
+        client = get_mongo_client()
+        tasks_collection = client.get_collection('tasks')
+        search_index_model = SearchIndexModel(
+            definition={
+                "mappings": {
+                    "dynamic": True
+                },
+            },
+            name="default",
+        )
+        result = tasks_collection.create_search_index(model=search_index_model)
+        print(result)
+    except Exception as e:
+        print(e)
+
+
+set_search_atlas_index()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##END POINTS
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -55,11 +91,11 @@ async def ping():
     conn = createDbConnection()
     return client_ping(conn)
 #test
-@app.get("/tasks")
+@app.get("/api/tasks")
 async def get_tasks():
-    client = get_client()
-    tasks_collection = client['sample_mflix']
-    tasks = list(tasks_collection.find({}).limit(10))
+    client = get_mongo_client()
+    tasks_collection = client.get_collection('tasks')
+    tasks = list(tasks_collection.find({}).limit(25))
     for task in tasks:
         task["_id"] = str(task["_id"])
     return tasks
